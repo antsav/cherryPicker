fabbd539dd5622df0eb517e094fe167336590df0
Anton <antsav@gmail.com>
2014-10-03 22:45:15 -0700

    non dynamic async parallel

--- core/printer.js
+++ core/printer.js
@@ -1,30 +1,41 @@
 // vendor libs - core
 var fs =                require('fs');
 var colors =            require('colors');
-var AsciiTable =        require('ascii-table')
+var AsciiTable =        require('ascii-table');
 
 var fileWriter = function (cherryAst) {
 
-    if (!fs.existsSync(__dirname + "/public/js/build")) {
-        fs.mkdir(__dirname + "/public/js/build");
+    if (!fs.existsSync("./public/js/build")) {
+        fs.mkdir("./public/js/build");
         console.log('build directory created'.blue);
     } else {
         console.log('overwriting files in build'.blue);
     }
 
-    var wstream = fs.createWriteStream(__dirname + '/public/js/build/lib.js');
+    var wstream = fs.createWriteStream('./public/js/build/lib.js');
     wstream.write(cherryAst, 'utf8');
-    // todo: fix error with callback
     wstream.end();
 }; //fileWiter
 
+var logTable = function (obj) {
+    var table = new AsciiTable('A Title');
+    table
+        .setHeading('', 'Name', 'Age')
+        .addRow(1, 'Bob', 52)
+        .addRow(2, 'John', 34)
+        .addRow(3, 'Jim', 83)
+
+    console.log(table.toString());
+}
+
 var logFinal = function (orBytes, comBytes) {
+//    console.log(orBytes, comBytes);
     console.log("The library compiled!".green);
     console.log(
-        "File size dropped from: " + colors.red(fileContent.length) +
-            " to: " + colors.green(cherryAst.length) + " symbols"
+        "File size dropped from: " + colors.red(orBytes) +
+            " to: " + colors.green(comBytes) + " bytes"
     );
-    var percent = 100 - (cherryAst.length * 100) / fileContent.length;
+    var percent = 100 - (comBytes * 100) / orBytes;
     var barSaved = Array(percent.toFixed(0) - 1).join("\u2588");
     var barUsed  = Array((100 - percent.toFixed(0)) - 1).join("\u2588");
     console.log(
@@ -33,8 +44,10 @@ var logFinal = function (orBytes, comBytes) {
             (barUsed).green + ' ' +
             ((100 - percent).toFixed() + '%').green
     );
+    logTable();
 }; //logger
 
 
 module.exports.fileWriter =     fileWriter;
+module.exports.logFinal =       logTable;
 module.exports.logFinal =       logFinal;
--- index.js
+++ index.js
@@ -3,7 +3,6 @@ var async =             require('async');
 var fs =                require('fs');
 var UglifyJS =          require('uglify-js');
 
-
 // local libs
 var printer =           require('./core/printer');
 
@@ -27,25 +26,88 @@ for (var i = 0 ; i < libraryFiles.length; i += 1) {
 }
 
 
-//--------------------------ASYNC---------------------start
-// functions array generator
+
+
+////--------------------------ASYNC---------------------start
+//functions array generator
 var libraryFilesFnArr = [];
+//for (var i = 0; i < libraryFiles.length; i += 1) {
+//    var idx = i; // hack
+//    libraryFilesFnArr[i] = function(callback) {
+//        fs.readFile(libraryFiles[idx],
+//            'utf8', function (err, result) {
+//                // Abstract Syntax Trees
+//                return callback(err, UglifyJS.parse(result))
+//            });
+//    }
+//} //for
+
 for (var i = 0; i < libraryFiles.length; i += 1) {
-    var idx = i; // hack
-    libraryFilesFnArr[i] = function(callback) {
-        fs.readFile(libraryFiles[idx],
+
+    libraryFilesFnArr[i] = function (callback) {
+        fs.readFile(libraryFiles[0],
             'utf8', function (err, result) {
-                // Abstract Syntax Trees
-                callback(err, UglifyJS.parse(result))
+                var ast = UglifyJS.parse(result);
+                return callback(null, ast);
             });
     }
 } //for
 
 
-async.parallel(libraryFilesFnArr, function(err, ast) {
-    if (err) { throw err; }
-    var cherryBody = [];
 
+async.parallel(
+[
+    function (callback) {
+        fs.readFile(libraryFiles[0],
+            'utf8', function (err, result) {
+                var ast = UglifyJS.parse(result);
+                return callback(null, ast);
+            });
+    }
+
+]
+    ,
+    //final callback
+    function(err, result) {
+    var fileOneOut = result[0].print_to_string();
+    var cherryBody = [];
+    console.log("---->" + result[0].body);
+
+    //cherryPicking
+    result[0].body.forEach(function (libFn, libIdx) {
+        projectGraph.forEach(function (projFn) {
+            if (libFn.name.name === projFn.name) {
+                console.log(libFn.name.name, projFn.name);
+                cherryBody.push(result[0].body[libIdx]);
+            }
+        });
+    });
+
+    var compressor = UglifyJS.Compressor({warnings:false}); //init compressor
+    result[0].body = cherryBody; // replacing with cherryBody
+    var cherryAst = result[0].transform(compressor).print_to_string();
+
+    printer.fileWriter(cherryAst);
+    printer.logFinal(
+        Buffer.byteLength(fileOneOut, 'utf8'),
+        Buffer.byteLength(cherryAst, 'utf8')
+    );
+});
+
+//--------------------------ASYNC---------------------stop
+
+
+
+
+
+//------------------------OLD APPROACH----------------start
+//
+//fs.readFile(libraryFile, 'utf8', function read(err, fileContent) {
+//    if (err) { throw err; }
+//
+//    var ast = UglifyJS.parse(fileContent); // Abstract Syntax Tree
+//    var cherryBody = [];
+//
 //    //cherryPicking
 //    ast.body.forEach(function (libFn, libIdx) {
 //        projectGraph.forEach(function (projFn) {
@@ -57,78 +119,16 @@ async.parallel(libraryFilesFnArr, function(err, ast) {
 //    });
 //
 //    var compressor = UglifyJS.Compressor({warnings:false}); //init compressor
-//    ast.body = cherryBody; // replacing with cherryBody
+//
+//    ast.body = cherryBody;
 //    var cherryAst = ast.transform(compressor).print_to_string();
 //
-//    // creating build directory
-//    if (!fs.existsSync(__dirname + "/public/js/build")) {
-//        fs.mkdir(__dirname + "/public/js/build");
-//        console.info("build directory created");
-//    } else {
-//        console.info("overwriting files in build");
-//    }
-//
-//
-//    // writing compiled result
-//    var wstream = fs.createWriteStream(__dirname + '/public/js/build/lib.js');
-//    wstream.write(cherryAst, 'utf8');
-//        console.log("The library compiled!".green);
-//        console.log(
-//            "File size dropped from: " + colors.red(fileContent.length) +
-//                " to: " + colors.green(cherryAst.length) + " symbols"
-//        );
-//        // output console
-//        var percent = 100 - (cherryAst.length * 100) / fileContent.length;
-//        var barSaved = Array(percent.toFixed(0) - 1).join('|');
-//        var barUsed  = Array((100 - percent.toFixed(0)) - 1).join('|');
-//        console.log(
-//            colors.red(barSaved) +
-//                colors.green(barUsed)
-//        );
-//        console.log(colors.blue(percent.toFixed(2) + '% saved'));
-//    wstream.end();
-});
-
-//--------------------------ASYNC---------------------stop
+//    printer.fileWriter(cherryAst);
+//    printer.logFinal(
+//        Buffer.byteLength(fileContent, 'utf8'),
+//        Buffer.byteLength(cherryAst, 'utf8')
+//    );
+//});
+//------------------------OLD APPROACH----------------stop
 
 
-
-
-fs.readFile(libraryFile, 'utf8', function read(err, fileContent) {
-    if (err) { throw err; }
-
-    var ast = UglifyJS.parse(fileContent); // Abstract Syntax Tree
-    var cherryBody = [];
-
-    //cherryPicking
-    ast.body.forEach(function (libFn, libIdx) {
-        projectGraph.forEach(function (projFn) {
-            if (libFn.name.name === projFn.name) {
-                console.log(libFn.name.name, projFn.name);
-                cherryBody.push(ast.body[libIdx]);
-            }
-        });
-    });
-
-    var compressor = UglifyJS.Compressor({warnings:false}); //init compressor
-
-    ast.body = cherryBody;
-    var cherryAst = ast.transform(compressor).print_to_string();
-
-    printer.fileWriter(cherryAst);
-
-
-
-});
-
-
-
-//
-//var table = new AsciiTable('A Title')
-//table
-//    .setHeading('', 'Name', 'Age')
-//    .addRow(1, 'Bob', 52)
-//    .addRow(2, 'John', 34)
-//    .addRow(3, 'Jim', 83)
-//
-//console.log(table.toString())
